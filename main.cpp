4a544bab2f9bed940eab0309b5cd994c4546f400
Aleksandr Rodionov <ravengothic.com>
2020-10-29 01:05:16 +0300

    Я совершенно без понятия что я делаю, но очень стараюсь

--- main.cpp
+++ main.cpp
@@ -1,46 +1,48 @@
 #include "vector"
 #include <iostream>
-#include <c++/4.8.3/sstream>
-#include <c++/4.8.3/fstream>
-#include <c++/4.8.3/iterator>
 
 using namespace std;
 
 struct inputParam {
-    int id;         // Идентификатор ордера
-    int instr;      // Идентификатор инструмента
-    int count;      // Количество в ордере
-    int amount;     // Оставшееся количество в ордере
-    int res1;       // Зарезервировано (всегда 0)
-    int action;     // Действие (0 - удаление, 1 - добавление, 2 – исполнение)
-    int vec;        // Направление (0 - покупка, 1 - продажа)
-    int custom;     // Цена ордера
-    int dealCost;   // Цена сделки
-    int res2;       // Зарезервировано (пустая запись)
-    int res3;       // Зарезервировано (пустая запись)
-    int date;       // Дата GMT+3 (%4Y%m%d)
-    int time;       // Время GMT+3 (%H%M%S%3N)
-    int flag;       // Битовый флаги - различные признаки записи, выраженные в десятичном представлении 64-битного числа
-    int deal;       // Идентификатор сделки (если нет то 0)
+    unsigned long id;    // 1. Идентификатор ордера
+    int instr;           // 2. Идентификатор инструмента
+    int count;           // 3. Количество в ордере
+    int amount;          // 4. Оставшееся количество в ордере
+    int res1;            // 5. Зарезервировано (всегда 0)
+    int action;          // 6. Действие (0 - удаление, 1 - добавление, 2 – исполнение)
+    int vec;             // 7. Направление (0 - покупка, 1 - продажа)
+    int custom;          // 8. Цена ордера
+    int dealCost;        // 9. Цена сделки
+    int res2;            // 10. Зарезервировано (пустая запись)
+    int res3;            // 11. Зарезервировано (пустая запись)
+    int date;            // 12. Дата GMT+3 (%4Y%m%d)
+    int time;            // 13. Время GMT+3 (%H%M%S%3N)
+    int flag;            // 14. Битовый флаги - различные признаки записи, выраженные в десятичном представлении 64-битного числа
+    int deal;            // 15. Идентификатор сделки (если нет то 0)
 };
 
+inputParam func(string *line) {
+    inputParam o{
+        static_cast<unsigned long>(stol(line[0])),
+    };
+    return o;
+};
 
 int main() {
-    freopen("test.log/test.log", "r", stdin);
-    inputParam line;
-    string tmp = "";
+    freopen("./test.log", "r", stdin);
+    string tmp;
     int index = 0;
+    string line[15];
 
     while(getline(cin, tmp, '\t')){
-        long long x = 25567940313089;
+        line[index] = tmp;
 
-        cout << x << endl;
-        index++;
-        if(index == 1) {
+        if(index == 14) {
             break;
         }
+        index++;
     }
-
+//    inputParam o = func(line);
     return 0;
 }
 
